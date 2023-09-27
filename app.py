8af4af29d12e4f63cea6881ad3702fda10040b5b
Abdulwahid Shariff <76592349+Abdlwhd@users.noreply.github.com>
2023-09-27 16:54:24 +0300

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import gradio as gr
 import tensorflow as tf
 import cv2
 
-title = "CoviDigiScan"
+title = "CovMediScanX"
 
 head = (
   "<center>"
@@ -14,15 +14,16 @@ head = (
 cnn = tf.keras.models.load_model("cnn_model.h5")
 
 def predict_input_image(img):
-    img = img.reshape(1, 500, 500, 1)
-    prediction = cnn.predict(img).tolist()[0]
-    class_names = ["Covid"]
-    if preds[i, 0] >= 0.5: 
-        return {class_names[i]: prediction[i] for i in range(1)}
-    else:  
-        return {class_names[i]: 1-prediction[i] for i in range(1)}
+    img = img.reshape(1, 299, 299, 1)
+    prediction = cnn.predict(img)
+    prediction[prediction <= 0.5] = 0
+    prediction[prediction > 0.5] = 1
+    if prediction == 0:
+        return "Normal"
+    elif prediction== 1:
+        return "Covid"
 
-image = gr.inputs.Image(shape=(500, 500), image_mode='L', invert_colors=False, source="upload")
+image = gr.inputs.Image(shape=(299, 299), image_mode='L', invert_colors=False, source="upload")
 label = gr.outputs.Label()
 iface = gr.Interface(fn=predict_input_image, inputs=image, outputs=label,title=title, description=head)
 iface.launch()
